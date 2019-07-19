782f0edb9bb66a016039e425ca3984bc0958d63a
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2019-07-19 13:55:52 +0100

    Fix premature contentrule action in test setup

--- collective/contentrules/setfield/tests/test_setfield.py
+++ collective/contentrules/setfield/tests/test_setfield.py
@@ -13,10 +13,6 @@ class SetFieldAction(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
 
-        self.portal.portal_setup.runAllImportStepsFromProfile(
-            "profile-collective.contentrules.setfield:tests", purge_old=False
-        )
-
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
         self.folder = api.content.create(
             type="Folder", title="folder", container=self.portal
@@ -25,12 +21,19 @@ class SetFieldAction(unittest.TestCase):
             type="Document", title="document", container=self.folder
         )
 
+        self.portal.portal_setup.runAllImportStepsFromProfile(
+            "profile-collective.contentrules.setfield:tests", purge_old=False
+        )
+
     def test_trigger_script_on_object_modified(self):
         document = self.document
 
         title_before_action = document.title
+        self.assertEqual(document.title, "document")
+
         notify(ObjectModifiedEvent(document))
 
         title_after_action = document.title
+        self.assertEqual(document.title, "Title set by SetField")
 
         self.assertNotEqual(title_before_action, title_after_action)
