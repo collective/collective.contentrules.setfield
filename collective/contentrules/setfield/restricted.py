48bf98ee1971668a57179681e14eb5c2aee98963
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2021-08-05 14:05:47 +0100

    Fix override of 'locals'

--- collective/contentrules/setfield/restricted.py
+++ collective/contentrules/setfield/restricted.py
@@ -27,10 +27,10 @@ class PyScript(object):
     def __init__(self, code):
         self.code = compile_restricted(code, "<string>", "exec")
 
-    def execute(self, locals=None, output=None):
+    def execute(self, my_locals=None, output=None):
         my_globals = ZopeGuards.get_safe_globals()
         my_globals["_getattr_"] = ZopeGuards.guarded_getattr
-        if locals is None:
-            locals = {}
-        exec(self.code, my_globals, locals)
-        return locals
+        if my_locals is None:
+            my_locals = {}
+        exec(self.code, my_globals, my_locals)
+        return my_locals
