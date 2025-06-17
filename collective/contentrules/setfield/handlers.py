08d4ff4d96e2c4c08d66bc2fd25bcd04fc7d9d53
Jon Pentland <instification@users.noreply.github.com>
2025-06-17 16:32:03 +0100

    Create install/uninstall profile. Don't trigger subscribers unless installed (#34)
    * Create install/uninstall profile. Don't trigger subscribers unless installed.
    
    * Don't fail when deleting site from zmi

--- collective/contentrules/setfield/handlers.py
+++ collective/contentrules/setfield/handlers.py
@@ -5,13 +5,26 @@ from Products.CMFCore.interfaces import IContentish, IFolderish
 from zope.interface.interfaces import ObjectEvent
 from zope.event import notify
 from zope.interface import implementer
-
+from plone import api
+from Products.CMFPlone.utils import get_installer
+from plone.api.exc import CannotGetPortalError
 
 def modified(event):
     """When an object is modified, fire the ParentModifiedEvent for its
     direct descendents.
     """
     obj = event.object
+    try:
+        portal = api.portal.get()
+    except CannotGetPortalError:
+        # If we can't get the portal object, we can't check if we're installed.
+        # This can happen for example when operating on the root from the zmi.
+        return
+    installer = get_installer(portal)
+    installed = installer.is_product_installed('collective.contentrules.setfield')
+    if not installed:
+        return
+
     if not (
         IContentish.providedBy(obj)
         or IComment.providedBy(obj)
--- collective/contentrules/setfield/interfaces.py
+++ collective/contentrules/setfield/interfaces.py
@@ -2,6 +2,7 @@
 from collective.contentrules.setfield import SetFieldMessageFactory as _
 from plone.supermodel import model
 from zope import schema
+from zope.interface import Interface
 from zope.interface.interfaces import IObjectEvent
 from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
 
@@ -15,6 +16,10 @@ terms = [
     SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in items
 ]
 
+
+class ISetFieldLayer(Interface):
+    """Browser layer for the package.
+    """
 
 class ISetFieldAction(model.Schema):
 
